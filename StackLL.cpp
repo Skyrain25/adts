138450e76f924c5f7f3b13c0c5d5eb6412d35091
Skyrain25 <shontellebarker@gmail.com>
2017-03-23 15:14:37 -0400

    Lab 6 Completed

--- StackLL.cpp
+++ StackLL.cpp
@@ -1,1 +1,53 @@
 #include "StackLL.h"
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class Stack::Node
+{
+  public:
+   int data = 0;
+   Node* link = nullptr;
+};
+
+
+Stack::~Stack()
+{
+	clear();
+}
+
+int Stack::size()
+{
+  return num_elements;
+}
+
+void Stack::push(int val)
+{
+  Node* newPtr = new Node{val};
+  newPtr->link = frontPtr;
+  frontPtr = newPtr;
+  num_elements++;
+ }
+ 
+ void Stack::pop()
+ {
+   Node* delPtr = frontPtr;
+   frontPtr = frontPtr->link;
+   delete delPtr;
+   num_elements--;
+ }
+ 
+ int Stack::top()
+ {
+  return frontPtr->data;
+ }
+ 
+ void Stack::clear()
+ {
+	 while(size() != 0)
+	 {
+		 pop();
+	}
+}
+
